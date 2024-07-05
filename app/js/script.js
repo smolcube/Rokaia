c47b9ee67667d129465cae9d4ff19bf2e45e5ae8
smolcube <itsrokaia.2016@gmail.com>
2024-07-05 18:56:10 +0200

    update

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,16 @@
-console.log('hi')
+document.addEventListener('DOMContentLoaded', function() {
+    const navItems = document.querySelectorAll('.nav-item');
+
+    navItems.forEach(item => {
+        item.addEventListener('click', function(event) {
+            event.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            const targetSection = document.getElementById(targetId);
+
+            window.scrollTo({
+                top: targetSection.offsetTop - 50, // Adjust 50 to the height of your fixed navbar if needed
+                behavior: 'smooth'
+            });
+        });
+    });
+});
