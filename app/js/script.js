67a8b121109e3a5e28078084a339f358a2a80f14
smolcube <itsrokaia.2016@gmail.com>
2024-07-08 01:27:56 +0200

    update

--- app/js/script.js
+++ app/js/script.js
@@ -14,3 +14,23 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const sections = document.querySelectorAll('section');
+    
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('active');
+        }
+      });
+    }, {
+      threshold: 0.5  // Adjust threshold as needed
+    });
+    
+    sections.forEach(section => {
+      observer.observe(section);
+    });
+  });
+  
